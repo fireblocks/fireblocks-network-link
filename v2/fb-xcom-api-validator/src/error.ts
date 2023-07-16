2a507fbb677c5b2d0af10293e7ddab457adac284
Yaniv Mordekhay <ymordekhay@fireblocks.com>
2023-07-16 11:29:16 +0300

    Improved error logging

--- v2/fb-xcom-api-validator/src/error.ts
+++ v2/fb-xcom-api-validator/src/error.ts
@@ -2,8 +2,8 @@ import VError from 'verror';
 
 export class XComError extends VError {
   constructor(
-    message: string,
-    public data?: Record<string, unknown>,
+    public readonly message: string,
+    public readonly data?: Record<string, unknown>,
     public readonly originalError?: Error | any
   ) {
     super({ cause: originalError, info: { ...data, ...originalError } }, message);
