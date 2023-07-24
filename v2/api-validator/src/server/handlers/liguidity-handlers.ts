03f1331cf490835f95c6e2826e210852c8d8fd32
Yaniv Mordekhay <ymordekhay@fireblocks.com>
2023-07-25 00:07:13 +0300

    Return 404 by default in empty handlers

--- v2/api-validator/src/server/handlers/liguidity-handlers.ts
+++ v2/api-validator/src/server/handlers/liguidity-handlers.ts
@@ -1,5 +1,9 @@
-import { Quote } from '../../client/generated';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { ErrorType, Quote } from '../../client/generated';
 
-export async function createQuote(): Promise<Quote> {
-  return {} as Quote;
+export async function createQuote(request: FastifyRequest, reply: FastifyReply): Promise<Quote> {
+  return reply.code(404).send({
+    message: 'Entity not found',
+    errorType: ErrorType.NOT_FOUND,
+  });
 }
--- v2/api-validator/src/server/handlers/transfer-handlers.ts
+++ v2/api-validator/src/server/handlers/transfer-handlers.ts
@@ -1,5 +1,12 @@
-import { BlockchainWithdrawal } from '../../client/generated';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { BlockchainWithdrawal, ErrorType } from '../../client/generated';
 
-export async function createBlockchainWithdrawal(): Promise<BlockchainWithdrawal> {
-  return {} as BlockchainWithdrawal;
+export async function createBlockchainWithdrawal(
+  request: FastifyRequest,
+  reply: FastifyReply
+): Promise<BlockchainWithdrawal> {
+  return reply.code(404).send({
+    message: 'Entity not found',
+    errorType: ErrorType.NOT_FOUND,
+  });
 }
