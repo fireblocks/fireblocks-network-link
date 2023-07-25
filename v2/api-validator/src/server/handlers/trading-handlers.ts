70f7a7754a9924368d028e1a3e73ba8911a33f9b
Yaniv Mordekhay <ymordekhay@fireblocks.com>
2023-07-25 16:19:08 +0300

    Added empty handlers for all trading endpoints

--- v2/api-validator/src/server/handlers/trading-handlers.ts
+++ v2/api-validator/src/server/handlers/trading-handlers.ts
@@ -1,7 +1,78 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
-import { ErrorType, Order } from '../../client/generated';
+import { ErrorType, MarketEntry, MarketTrade, Order, OrderBook } from '../../client/generated';
+
+export async function getBooks(request: FastifyRequest, reply: FastifyReply): Promise<OrderBook[]> {
+  return reply.code(404).send({
+    message: 'Entity not found',
+    errorType: ErrorType.NOT_FOUND,
+  });
+}
+
+export async function getBookDetails(
+  request: FastifyRequest,
+  reply: FastifyReply
+): Promise<OrderBook> {
+  return reply.code(404).send({
+    message: 'Entity not found',
+    errorType: ErrorType.NOT_FOUND,
+  });
+}
+
+export async function getBookAsks(
+  request: FastifyRequest,
+  reply: FastifyReply
+): Promise<MarketEntry[]> {
+  return reply.code(404).send({
+    message: 'Entity not found',
+    errorType: ErrorType.NOT_FOUND,
+  });
+}
+
+export async function getBookBids(
+  request: FastifyRequest,
+  reply: FastifyReply
+): Promise<MarketEntry[]> {
+  return reply.code(404).send({
+    message: 'Entity not found',
+    errorType: ErrorType.NOT_FOUND,
+  });
+}
+
+export async function getBookOrderHistory(
+  request: FastifyRequest,
+  reply: FastifyReply
+): Promise<MarketTrade[]> {
+  return reply.code(404).send({
+    message: 'Entity not found',
+    errorType: ErrorType.NOT_FOUND,
+  });
+}
+
+export async function getOrders(request: FastifyRequest, reply: FastifyReply): Promise<Order[]> {
+  return reply.code(404).send({
+    message: 'Entity not found',
+    errorType: ErrorType.NOT_FOUND,
+  });
+}
 
 export async function createOrder(request: FastifyRequest, reply: FastifyReply): Promise<Order> {
+  return reply.code(404).send({
+    message: 'Entity not found',
+    errorType: ErrorType.NOT_FOUND,
+  });
+}
+
+export async function getOrderDetails(
+  request: FastifyRequest,
+  reply: FastifyReply
+): Promise<Order> {
+  return reply.code(404).send({
+    message: 'Entity not found',
+    errorType: ErrorType.NOT_FOUND,
+  });
+}
+
+export async function cancelOrder(request: FastifyRequest, reply: FastifyReply): Promise<void> {
   return reply.code(404).send({
     message: 'Entity not found',
     errorType: ErrorType.NOT_FOUND,
