4685ef52c4d6cdee76c45cd8a728b91242e450be
Yaniv Mordekhay <ymordekhay@fireblocks.com>
2023-07-26 19:39:55 +0300

    Fix account routes registration

--- v2/api-validator/src/server/handlers/index.ts
+++ v2/api-validator/src/server/handlers/index.ts
@@ -1,5 +1,6 @@
 export { getAdditionalAssets } from './additional-assets-handler';
 export { getAssetDetails } from './asset-details-handler';
+export * from './account-handlers';
 export * from './trading-handlers';
 export { getCapabilities } from './capabilities-handler';
 export { createBlockchainWithdrawal } from './transfer-handlers';
