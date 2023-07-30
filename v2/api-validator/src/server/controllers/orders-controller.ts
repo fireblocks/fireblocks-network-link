3280560b92c0d9a0c936269dbf3836c76a93e730
Yaniv Mordekhay <ymordekhay@fireblocks.com>
2023-07-30 17:32:16 +0300

    OrdersController

---
+++ v2/api-validator/src/server/controllers/orders-controller.ts
@@ -0,0 +1,86 @@
+import { randomUUID } from 'crypto';
+import { XComError } from '../../error';
+import { books } from './books-controller';
+import { Order, OrderData, OrderRequest, OrderStatus } from '../../client/generated';
+import _ from 'lodash';
+
+type StoredOrder = { order: Order; idempotencyKey: string };
+
+export class OrdersController {
+  private readonly usedIdempotencyKeys = new Set<string>();
+  private readonly orders: Array<StoredOrder> = [];
+
+  public createOrder(order: OrderRequest): Order {
+    const book = books.find((b) => b.id === order.bookId);
+    if (!book) {
+      throw new UnknownBookError(order.bookId);
+    }
+
+    const orderData = _.omit(order, 'idempotencyKey') as OrderData;
+
+    const idempotentOrder = this.orders.find((o) => o.idempotencyKey === order.idempotencyKey);
+    if (idempotentOrder) {
+      if (_.isMatch(idempotentOrder.order, orderData)) {
+        return idempotentOrder.order;
+      } else {
+        throw new IdempotencyKeyReuseError(order.idempotencyKey);
+      }
+    }
+
+    const o: Order = {
+      ...orderData,
+      id: randomUUID(),
+      status: OrderStatus.TRADING,
+      trades: [],
+      createdAt: new Date().toISOString(),
+    };
+    this.orders.push({ order: o, idempotencyKey: order.idempotencyKey });
+    return o;
+  }
+
+  public getOrdersCount(): number {
+    return this.orders.length;
+  }
+
+  public findOrder(orderId: string): Order | undefined {
+    const stored = this.orders.find((o) => o.order.id === orderId);
+    return stored?.order;
+  }
+
+  public cancelOrder(orderId: string): void {
+    const order = this.findOrder(orderId);
+    if (!order) {
+      throw new UnknownOrderError(orderId);
+    }
+
+    if (order.status !== OrderStatus.TRADING) {
+      throw new CannotCancelOrder(orderId, order.status);
+    }
+
+    order.status = OrderStatus.CANCELED;
+  }
+}
+
+export class UnknownBookError extends XComError {
+  constructor(public readonly book: string) {
+    super('Unknown book', { book });
+  }
+}
+
+export class UnknownOrderError extends XComError {
+  constructor(public readonly id: string) {
+    super('Unknown order', { id });
+  }
+}
+
+export class CannotCancelOrder extends XComError {
+  constructor(public readonly id: string, public readonly status: OrderStatus) {
+    super('Cannot cancel order in this state', { id, status });
+  }
+}
+
+export class IdempotencyKeyReuseError extends XComError {
+  constructor(public readonly key: string) {
+    super('Idempotency key was already used', { key });
+  }
+}
---
+++ v2/api-validator/tests/self-tests/orders-controller.test.ts
@@ -0,0 +1,134 @@
+import { randomUUID } from 'crypto';
+import { books } from '../../src/server/controllers/books-controller';
+import {
+  CannotCancelOrder,
+  IdempotencyKeyReuseError,
+  OrdersController,
+  UnknownBookError,
+  UnknownOrderError,
+} from '../../src/server/controllers/orders-controller';
+import {
+  Order,
+  OrderData,
+  OrderSide,
+  OrderStatus,
+  OrderTimeInForce,
+  OrderType,
+} from '../../src/client/generated';
+
+const book = books[0];
+
+const orderDetails: OrderData = {
+  bookId: book.id,
+  side: OrderSide.BUY,
+  orderType: OrderType.LIMIT,
+  timeInForce: OrderTimeInForce.GOOD_TILL_CANCELED,
+  baseAssetPrice: '1.2',
+  baseAssetQuantity: 10,
+};
+
+describe('Create order', () => {
+  let controller: OrdersController;
+
+  beforeEach(() => {
+    controller = new OrdersController();
+  });
+
+  describe('Order for an existing book', () => {
+    let order: Order;
+
+    beforeEach(() => {
+      order = controller.createOrder({ ...orderDetails, idempotencyKey: randomUUID() });
+    });
+
+    it('should copy the data from the request object', () => {
+      expect(order).toMatchObject(orderDetails);
+    });
+    it('should have id', () => {
+      expect(order.id).not.toBeEmpty();
+    });
+    it('should have status "trading"', () => {
+      expect(order.status).toEqual(OrderStatus.TRADING);
+    });
+    it('should have creation date', () => {
+      expect(order.createdAt).toBeDateString();
+    });
+  });
+
+  describe('Order for a non existing book', () => {
+    it('should throw UnknownBookError', () => {
+      const badOrderDetails: OrderData = { ...orderDetails, bookId: 'USD_XLM' };
+
+      expect(() =>
+        controller.createOrder({ ...badOrderDetails, idempotencyKey: randomUUID() })
+      ).toThrow(UnknownBookError);
+    });
+  });
+
+  describe('Idempotency key reuse', () => {
+    const idempotencyKey = randomUUID();
+    let originalOrder: Order;
+
+    beforeEach(() => {
+      originalOrder = controller.createOrder({ ...orderDetails, idempotencyKey });
+    });
+
+    describe('Identical requests', () => {
+      let newOrder: Order;
+
+      beforeEach(() => {
+        newOrder = controller.createOrder({ ...orderDetails, idempotencyKey });
+      });
+
+      it('should return the original order', () => {
+        expect(newOrder).toEqual(originalOrder);
+      });
+      it('should not create new records', () => {
+        expect(controller.getOrdersCount()).toEqual(1);
+      });
+    });
+
+    describe('Different requests', () => {
+      it('should throw IdempotencyKeyReuseError', () => {
+        const badOrderDetails: OrderData = { ...orderDetails, side: OrderSide.SELL };
+
+        expect(() => controller.createOrder({ ...badOrderDetails, idempotencyKey })).toThrow(
+          IdempotencyKeyReuseError
+        );
+      });
+    });
+  });
+});
+
+describe('Cancel order', () => {
+  let controller: OrdersController;
+  let orderId: string;
+
+  beforeEach(() => {
+    controller = new OrdersController();
+    const order = controller.createOrder({ ...orderDetails, idempotencyKey: randomUUID() });
+    orderId = order.id;
+  });
+
+  describe('Order is trading', () => {
+    it('should cancel', () => {
+      controller.cancelOrder(orderId);
+      const order = controller.findOrder(orderId);
+      expect(order).toBeDefined();
+      expect(order?.status).toEqual(OrderStatus.CANCELED);
+    });
+  });
+
+  describe('Unknown order', () => {
+    it('should throw UnknownOrderError', () => {
+      expect(() => controller.cancelOrder(randomUUID())).toThrow(UnknownOrderError);
+    });
+  });
+
+  describe('Cancel order twice', () => {
+    it('should throw CannotCancelOrder', () => {
+      controller.cancelOrder(orderId);
+      expect(() => controller.cancelOrder(orderId)).toThrow(CannotCancelOrder);
+    });
+  });
+});
