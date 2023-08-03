4914a17b7028da55890a39def2fc11421c85a726
Yaniv Mordekhay <ymordekhay@fireblocks.com>
2023-08-03 15:44:27 +0300

    [OpenAPI] Added optional minWithdrawalAmount to WithdrawalCapability

--- v2/api-validator/src/client/generated/models/WithdrawalCapability.ts
+++ v2/api-validator/src/client/generated/models/WithdrawalCapability.ts
@@ -4,14 +4,16 @@
 /* eslint-disable */
 
 import type { AssetReference } from './AssetReference';
+import type { PositiveAmount } from './PositiveAmount';
 import type { TransferCapability } from './TransferCapability';
 
 /**
- * Capability to withdraw a balance asset using a specific transfer capability.
+ * Capability to withdraw a balance asset using a specific transfer capability. `minWithdrawalAmount` is the minimum amount of the balance currency that can be withdrawn using this capability. If `minWithdrawalAmount` is not specified, it will be assumed that the minimum withdrawal amount is zero.
  */
 export type WithdrawalCapability = {
     id: string;
     withdrawal: TransferCapability;
     balanceAsset: AssetReference;
+    minWithdrawalAmount?: PositiveAmount;
 };
 
