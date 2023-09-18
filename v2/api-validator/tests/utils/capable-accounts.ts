6f744de93425bd017f880a31b7aa5bbff5e60055
Yaniv Mordekhay <ymordekhay@fireblocks.com>
2023-09-18 16:16:46 +0300

    PIK-3577 Trading tests: use preloaded capabilities

--- v2/api-validator/tests/utils/capable-accounts.ts
+++ v2/api-validator/tests/utils/capable-accounts.ts
@@ -1,12 +1,61 @@
-import Client from '../../src/client';
-import { Account } from '../../src/client/generated';
+import ApiClient from '../../src/client';
 import { Pageable, paginated } from './pagination';
+import { Account, AccountStatus, ApiComponents } from '../../src/client/generated';
+
+export function hasCapability(component: keyof ApiComponents): boolean {
+  const accountId = findCapableAccount(component);
+  return accountId != null;
+}
+
+export function getCapableAccountId(component: keyof ApiComponents): string {
+  const accountId = findCapableAccount(component);
+  if (accountId == null) {
+    throw new Error(`No ${component} capability`);
+  }
+  return accountId;
+}
+
+export function findCapableAccount(component: keyof ApiComponents): string | undefined {
+  const capability = ApiClient.getCachedApiComponents()[component];
+  if (!capability) {
+    return undefined;
+  }
+  if (capability === '*') {
+    return getAnyActiveAccountId();
+  }
+  if (!Array.isArray(capability)) {
+    throw new Error('Unexpected capability value: ' + capability);
+  }
+
+  for (const accountId of capability) {
+    if (isActiveAccount(accountId)) {
+      return accountId;
+    }
+  }
+}
+
+function getAnyActiveAccountId(): string | undefined {
+  for (const account of ApiClient.getCachedAccounts()) {
+    if (account.status === AccountStatus.ACTIVE) {
+      return account.id;
+    }
+  }
+}
+
+function isActiveAccount(accountId: string): boolean {
+  for (const account of ApiClient.getCachedAccounts()) {
+    if (account.id === accountId) {
+      return account.status === AccountStatus.ACTIVE;
+    }
+  }
+  return false;
+}
 
 export async function getCapableAccounts(
   capability: '*' | string[],
   balances?: boolean
 ): Promise<Account[]> {
-  const client = new Client();
+  const client = new ApiClient();
 
   if (Array.isArray(capability)) {
     return await getAccountsFromIdList(capability);
@@ -27,7 +76,7 @@ export async function getCapableAccounts(
 }
 
 async function getAccountsFromIdList(accountIds: string[]): Promise<Account[]> {
-  const client = new Client();
+  const client = new ApiClient();
   const accountPromises: Promise<Account>[] = [];
 
   for (const accountId of accountIds) {
