9880f354759c28fdd3349d5d3414aad01756e8d3
Chris Brand <cbrand@luno.com>
2023-11-07 11:58:25 +0200

    fix linting

--- v2/api-validator/tests/server-tests/pagination-params.test.ts
+++ v2/api-validator/tests/server-tests/pagination-params.test.ts
@@ -37,7 +37,7 @@ describe('Pagination params tests', () => {
 
       try {
         if (params !== undefined) {
-          params.accountId = getCapableAccountId(schema.tags[0] as keyof ApiComponents)
+          params.accountId = getCapableAccountId(schema.tags[0] as keyof ApiComponents);
         }
       } catch (e) {
         // no capable account, use faked account
