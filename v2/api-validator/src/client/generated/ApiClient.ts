6a3b7738d263faf1e66c58db9cd3176fa87db01a
Yaniv Mordekhay <ymordekhay@fireblocks.com>
2024-05-20 12:59:13 +0300

    Prepare APIv2 release 0.4.1

--- v2/api-validator/src/client/generated/ApiClient.ts
+++ v2/api-validator/src/client/generated/ApiClient.ts
@@ -39,7 +39,7 @@ export class ApiClient {
     constructor(config?: Partial<OpenAPIConfig>, HttpRequest: HttpRequestConstructor = AxiosHttpRequest) {
         this.request = new HttpRequest({
             BASE: config?.BASE ?? 'http://0.0.0.0:8000',
-            VERSION: config?.VERSION ?? '0.3.1',
+            VERSION: config?.VERSION ?? '0.4.1',
             WITH_CREDENTIALS: config?.WITH_CREDENTIALS ?? false,
             CREDENTIALS: config?.CREDENTIALS ?? 'include',
             TOKEN: config?.TOKEN,
