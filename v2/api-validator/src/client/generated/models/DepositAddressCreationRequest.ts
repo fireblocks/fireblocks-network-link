6c43fdb8d3623ccee081bbe426835eeaef6f0c82
Yarden Even <yeven@fireblocks.com>
2025-02-03 13:57:09 +0200

    PIK-7527 remove for creation

--- v2/api-validator/src/client/generated/models/DepositAddressCreationRequest.ts
+++ v2/api-validator/src/client/generated/models/DepositAddressCreationRequest.ts
@@ -4,13 +4,11 @@
 /* eslint-disable */
 
 import type { IbanCapability } from './IbanCapability';
-import type { InternalTransferCapability } from './InternalTransferCapability';
-import type { PeerAccountTransferCapability } from './PeerAccountTransferCapability';
 import type { PublicBlockchainCapability } from './PublicBlockchainCapability';
 import type { SwiftCapability } from './SwiftCapability';
 
 export type DepositAddressCreationRequest = {
     idempotencyKey: string;
-    transferMethod: (PublicBlockchainCapability | IbanCapability | SwiftCapability | PeerAccountTransferCapability | InternalTransferCapability);
+    transferMethod: (PublicBlockchainCapability | IbanCapability | SwiftCapability);
 };
 
