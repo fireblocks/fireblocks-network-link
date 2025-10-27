acdca45a7e02c19eacf019c64241b9adfdd72494
Shaked Rosenberg <shaked@fireblocks.com>
2025-10-27 12:16:04 +0200

    Address prefunded option in initial creation status test

--- v2/api-validator/tests/server-tests/ramps.test.ts
+++ v2/api-validator/tests/server-tests/ramps.test.ts
@@ -404,8 +404,13 @@ describe.skipIf(noRampsCapability)('Ramps', () => {
         ).toEqual(capability.from.asset);
       });
 
-      it('should receive initial status PENDING', async () => {
-        expect(createdRamp.status).toBe(RampStatus.PENDING);
+      it('should receive initial status based on the ramp type', async () => {
+        const isPrefunded = 'type' in capability.from && capability.from.type === 'Prefunded';
+        if (isPrefunded) {
+          expect(createdRamp.status).toBe(RampStatus.PROCESSING);
+        } else {
+          expect(createdRamp.status).toBe(RampStatus.PENDING);
+        }
       });
 
       it('should find ramp in details endpoint', async () => {
