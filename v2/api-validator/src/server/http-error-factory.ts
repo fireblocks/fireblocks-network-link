38027ba82bc66c795ffb781b87fab008a1a539f3
Meir Rotstein <mrotstein@fireblocks.com>
2026-03-24 11:44:35 +0200

    FA-6575 extend errorTypes with additinal options

--- v2/api-validator/src/server/http-error-factory.ts
+++ v2/api-validator/src/server/http-error-factory.ts
@@ -28,3 +28,59 @@ export function idempotencyKeyReuse(reply: FastifyReply): FastifyReply {
   };
   return reply.code(400).send(errorData);
 }
+
+export function unsupportedBaseAsset(reply: FastifyReply): FastifyReply {
+  const errorData: BadRequestError = {
+    message: 'The base asset is not supported by the provider',
+    errorType: BadRequestError.errorType.UNSUPPORTED_BASE_ASSET,
+  };
+  return reply.code(400).send(errorData);
+}
+
+export function unsupportedQuoteAsset(reply: FastifyReply): FastifyReply {
+  const errorData: BadRequestError = {
+    message: 'The quote asset is not supported by the provider',
+    errorType: BadRequestError.errorType.UNSUPPORTED_QUOTE_ASSET,
+  };
+  return reply.code(400).send(errorData);
+}
+
+export function amountBelowMinimum(reply: FastifyReply): FastifyReply {
+  const errorData: BadRequestError = {
+    message: 'The requested amount is below the provider minimum',
+    errorType: BadRequestError.errorType.AMOUNT_BELOW_MINIMUM,
+  };
+  return reply.code(400).send(errorData);
+}
+
+export function piiMissing(reply: FastifyReply): FastifyReply {
+  const errorData: BadRequestError = {
+    message: 'Required KYC / PII information is missing',
+    errorType: BadRequestError.errorType.PII_MISSING,
+  };
+  return reply.code(400).send(errorData);
+}
+
+export function unsupportedExternalSource(reply: FastifyReply): FastifyReply {
+  const errorData: BadRequestError = {
+    message: 'Provider requires source to be internal to Fireblocks for screening',
+    errorType: BadRequestError.errorType.UNSUPPORTED_EXTERNAL_SOURCE,
+  };
+  return reply.code(400).send(errorData);
+}
+
+export function unsupportedRegion(reply: FastifyReply): FastifyReply {
+  const errorData: BadRequestError = {
+    message: 'Provider does not support orders from the customer region',
+    errorType: BadRequestError.errorType.UNSUPPORTED_REGION,
+  };
+  return reply.code(400).send(errorData);
+}
+
+export function destinationNotWhitelisted(reply: FastifyReply): FastifyReply {
+  const errorData: BadRequestError = {
+    message: 'The destination address must be whitelisted with the provider first',
+    errorType: BadRequestError.errorType.DESTINATION_NOT_WHITELISTED,
+  };
+  return reply.code(400).send(errorData);
+}
